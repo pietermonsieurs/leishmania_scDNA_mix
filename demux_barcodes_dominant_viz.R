797603839eee93ae7e8e382fd2d9854be3eca330
Pieter Monsieurs <pmonsieurs@itg.be>
2026-05-27 16:14:15 +0200

    script for viz of demux results

---
+++ demux_barcodes_dominant_viz.R
@@ -0,0 +1,95 @@
+library(ggplot2)
+library(ComplexHeatmap)
+library(reshape2)
+
+## parameter settings
+data_dir = '/Users/pmonsieurs/programming/leishmania_scDNA_mix/results/demux/'
+barcode_count_file = paste0(data_dir, 'dominant_barcodes_merged_detailed.txt')
+barcode_df <- read.table(barcode_count_file, header=TRUE)
+
+colnames(barcode_df)[-1] <- gsub("_MKDL2600063...1A_23HK37LT4", "", colnames(barcode_df)[-1])
+colnames(barcode_df)[-1] <- gsub("_Undetermined_23HK37LT4", "", colnames(barcode_df)[-1])
+head(barcode_df)
+dim(barcode_df)
+
+## create empty dataframe to store the overlap between the different samples. As
+## column names, drop the first column (barcode) and last 2 columns (barcode1 and encapsulation)
+overlap <- matrix(0, nrow = ncol(barcode_df) - 3, ncol = ncol(barcode_df) - 3)
+colnames(overlap) <- colnames(barcode_df)[2:(ncol(barcode_df)-2)]
+rownames(overlap) <- colnames(barcode_df)[2:(ncol(barcode_df)-2)]
+
+table(barcode_df$encapsulation)
+
+## create a heatmap with the overlap between the different samples
+combi = 0
+for (sample1 in colnames(barcode_df)[-1]) {
+  for (sample2 in colnames(barcode_df)[-1]) {
+    combi = combi + 1
+    overlap_count <- sum(barcode_df[[sample1]] > 0 & barcode_df[[sample2]] > 0)
+    overlap[sample1, sample2] <- overlap_count
+    print(paste("count combi", combi, sample1, sample2, overlap_count))
+  }
+}
+
+## create a heatmap with the overlap between the different samples. Also show
+## the number of overlapping barcodes
+dist_mat <- as.dist(1 - (overlap / max(overlap)))
+Heatmap(overlap,
+        name = "Overlap",
+        clustering_distance_rows = dist_mat,
+        clustering_distance_columns = dist_mat,
+        #col = colorRamp2(c(0, max(overlap)), c("white", "red")),
+        cluster_rows = TRUE,
+        cluster_columns = TRUE,
+        show_row_names = TRUE,
+        show_column_names = TRUE,
+        cell_fun = function(j, i, x, y, width, height, fill) {
+          grid.text(overlap[i, j], x, y)
+        })
+
+
+## create a heatmap with all barcode
+Heatmap(log10(barcode_df[,-1]+1),
+        name = "Barcode Count",
+        #col = colorRamp2(c(0, max(barcode_df[,-1])), c("white", "red")),
+        cluster_rows = TRUE,
+        cluster_columns = TRUE,
+        show_row_names = FALSE,
+        show_column_names = TRUE)
+
+
+
+
+## check the spread of the enxapsulation over the different libraries / samples
+## using a stacked bar plot
+barcode_long <- melt(
+  barcode_df,
+  id.vars = c("barcode", "barcode1", "encapsulation"),
+  measure.vars = 2:11,
+  variable.name = "sample",
+  value.name = "count"
+)
+
+## counting all the barcode separately
+ggplot(barcode_long, aes(x = sample, y = count, fill = encapsulation)) +
+  geom_bar(stat = "identity", position = "stack") +
+  theme_minimal() +
+  labs(title = "Distribution of Encapsulation Types Across Samples",
+       x = "Sample",
+       y = "Count of Barcodes") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+
+## same barplot, but count rather the number of unique barcode instead of taking
+## the count of the barcode into account
+barcode_long_unique <- barcode_long %>%
+  group_by(sample, encapsulation) %>%
+  summarise(unique_barcodes = sum(count > 0))
+
+ggplot(barcode_long_unique, aes(x = sample, y = unique_barcodes, fill = encapsulation)) +
+  geom_bar(stat = "identity", position = "stack") +
+  theme_minimal() +
+  labs(title = "Distribution of Unique Barcodes Across Samples",
+       x = "Sample",
+       y = "Number of Unique Barcodes") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
